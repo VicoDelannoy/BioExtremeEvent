b77d1700e9c8d88a0b5367f4aad4d9f428ac67e7
VicoDelannoy <valefvre@gmail.com>
2026-01-18 17:45:21 +0100

    Add test on the dataset provided in bee_merge_summarize (spatial overlapping), unfinnisf because of GEOS library.

--- R/bee_merge_summarize.R
+++ R/bee_merge_summarize.R
@@ -2,91 +2,270 @@
 #'
 #'@description
 #'
-#'@param data_metrics_point the output of the BEE.calc.metrics_point computed 
+#'@param data_metrics_point the output of the BEE.calc.metrics_point computed
 #' using the argument group_by_event = FALSE.
-#' 
-#'@param data_metrics_morpho the output of the BEE.calc.metrics_morpho 
-#' 
-#'@param data_escape the output of the BEE.calc.escape computed 
+#'
+#'@param data_metrics_morpho the output of the BEE.calc.metrics_morpho
+#'
+#'@param data_escape the output of the BEE.calc.escape computed
 #' using the argument group_by_event = FALSE.
-#' 
+#'
 #'@param summarize_by takes the followings options, into "" :
-#' - "extrem_event" for each metrics, a mean, median, variance, min and max will
+#' - "extreme_event" for each metrics, a mean, median, variance, min and max will
 #' be computed for each extrem event
+#' - "day" metrics are not summarized through time, the function keeps a daily 
+#' resolution and just merge the datasets.
 #' - "week" for each metrics, a mean, median, variance, min and max will
-#' be computed weekly. Note that this imply to provide daily output 
+#' be computed weekly. Note that this imply to provide daily output
 #' (BEE.calc.escape(only_days_EE = FALSE))
 #' - "two_weeks" for each metrics, a mean, median, variance, min and max will
-#' be computed every two weeks. Note that this imply to provide daily output 
+#' be computed every two weeks. Note that this imply to provide daily output
 #' (BEE.calc.escape(only_days_EE = FALSE))
 #' - "month" for each metrics, a mean, median, variance, min and max will
-#' be computed monthly. Note that this imply to provide daily output 
+#' be computed monthly. Note that this imply to provide daily output
 #' (BEE.calc.escape(only_days_EE = FALSE))
 #' - "year" for each metrics, a mean, median, variance, min and max will
-#' be computed yearly. Note that this imply to provide daily output 
+#' be computed yearly. Note that this imply to provide daily output
 #' (BEE.calc.escape(only_days_EE = FALSE))
-#'@return 
 #' 
+#'@param crs a METRICS crs that suits the studdied area
+#' 
+#'@return
+#'
 #'@examples
 #'
 #'@export
 #-------------------------------------------------------------------------------
 
-# data_metrics_point <- points_metrics ; 
-# data_metrics_morpho <- list_morpho_metrics ; 
-# data_escape <- dist_to_escape
+# data_metrics_point <- points_metrics ;
+# data_metrics_morpho <- list_morpho_metrics ;
+# data_escape <- dist_to_escape ; crs = "EPSG:3035"
 
 BEE.merge_summarize <- function(
   data_metrics_point = NULL,
   data_metrics_morpho = NULL,
   data_escape = NULL,
-  summarize_by = "extrem_event"
+  summarize_by,
+  crs
 ) {
-# Identify which datasets have been provided
+  ################## FORMAT TESTS AND WARNINGS ###################################
+  # Check that the summarize_by is a valid option 
+  # ("extreme_event"/"weak"/"month"/"year") :
+  if(summarize_by !%in% c("extreme_event","day","weak","month","year"))
+    {warnings("The summarize_by argument is not a suitable option, please 
+    provide one from the following list : extreme_event, day, weak, month, year")}
+  # Identify which datasets have been provided :
   ## Is there at least two datasets provided ?
-  check_1 <- paste0(is.null(data_metrics_point),
-                   is.null(data_metrics_morpho),
-                   is.null(data_escape))
+  check_1 <- paste0(
+    is.null(data_metrics_point),
+    is.null(data_metrics_morpho),
+    is.null(data_escape)
+  )
 
-  if(check_1 == "TRUETRUETRUE"){
-    warnings("You haven't provided any data to be merged and summarised. Please
-    provide at least two datasets.")
+  if (check_1 == "TRUETRUETRUE") {
+    warnings(
+      "You haven't provided any data to be merged and summarised. Please
+    provide at least two datasets."
+    )
   }
-  if(check_1 == "FALSETRUETRUE"){
-    message("As you have only provided the metrics_point dataset, 
-    summarising through time is all that will be done.")
+  if (check_1 == "FALSETRUETRUE") {
+    message(
+      "As you have only provided the metrics_point dataset, 
+    summarising through time is all that will be done."
+    )
   }
-  if(check_1 == "TRUEFALSETRUE"){
-    message("As you have only provided the metrics_morpho dataset, 
-    summarising through time is all that will be done.")
+  if (check_1 == "TRUEFALSETRUE") {
+    message(
+      "As you have only provided the metrics_morpho dataset, 
+    summarising through time is all that will be done."
+    )
   }
-  if(check_1 == "TRUETRUEFALSE"){
-    message("As you have only provided the escape dataset, 
-    summarising through time is all that will be done.")
+  if (check_1 == "TRUETRUEFALSE") {
+    message(
+      "As you have only provided the escape dataset, 
+    summarising through time is all that will be done."
+    )
   }
-  
+
   ## Is it a daily resolution ?
-  if(!is.null(data_metrics_point)){
-    dates <- data_metrics_point[[1]]$Date
-    if(length(dates)!=dates[length(dates)]-dates[1]+1){
-      warnings("Some days are missing in data_metrics_point or the time 
+  if (!is.null(data_metrics_point)) {
+    dates_point <- as.Date(data_metrics_point[[1]]$Date)
+    if (
+      length(dates_point) !=
+        dates_point[length(dates_point)] - dates_point[1] + 1
+    ) {
+      warnings(
+        "Some days are missing in data_metrics_point or the time 
       resolution is not daily. Please, make sure to use 'group_by_event = FALSE'
-      when running BEE.calc.metrics_point")
+      when running BEE.calc.metrics_point"
+      )
     }
   }
-  if(!is.null(data_metrics_morpho)){
-    data_metrics_morpho <- data_metrics_morpho[[2]]
-    dates <- data_metrics_morpho[ vapply(data_metrics_morpho, ncol, integer(1)) == 6 ][[1]]$date
-    dates <- as.Date(dates)
-    if(length(dates)!=dates[length(dates)]-dates[1]+1){
-      warnings("Some days are missing in data_metrics_point or the time 
+  if (!is.null(data_metrics_morpho)) {
+    data_metrics_morpho <- data_metrics_morpho[[1]]
+    dates_morpho <- unique(data_metrics_morpho$date)
+    dates_morpho <- as.Date(dates_morpho)
+    if (
+      length(dates_morpho) !=
+        dates_morpho[length(dates_morpho)] - dates_morpho[1] + 1
+    ) {
+      warnings(
+        "Some days are missing in data_metrics_point or the time 
       resolution is not daily. Please, make sure to use 'group_by_event = FALSE'
-      when running BEE.calc.metrics_point")
+      when running BEE.calc.metrics_point"
+      )
     }
   }
-  if(!is.null(data_escape)){
-    # TO COMPLET
+  if (!is.null(data_escape)) {
+    dates_escape <- unique(data_escape$date)
+    dates_escape <- as.Date(dates_escape)
+    if (
+      length(dates_escape) !=
+        dates_escape[length(dates_escape)] - dates_escape[1] + 1
+    ) {
+      warnings(
+        "Some days are missing in data_metrics_point or the time 
+      resolution is not daily. Please, make sure to use 'group_by_event = FALSE'
+      when running BEE.calc.metrics_point"
+      )
+    }
   }
-  # tester homogénéité des périodes fournies
-  # tester homogénéité spatiale
+  ## Are the time periods overlapping ?
+  if (check_1 == "FALSEFALSETRUE") {
+    shared <- length(intersect(dates_point, dates_morpho))
+    if (shared == 0) {
+      warnings(
+        "data_metrics_point and data_metrics_morpho do not share the same
+      time period (not a single day in common). Please provide data sharing some
+      dates in common and comming from the same rasters."
+      )
+    }
+    if (shared < length(dates_point)) {
+      message(
+        "data_metrics_point covers more dates than data_metrics_morpho, 
+      the merging will only be done on the days they share."
+      )
+    }
+    if (shared < length(dates_morpho)) {
+      message(
+        "data_metrics_morpho covers more dates than data_metrics_point, 
+      the merging will only be done on the days they share."
+      )
+    }
+  }
+  if (check_1 == "FALSETRUEFALSE") {
+    shared <- length(intersect(dates_point, dates_escape))
+    if (shared == 0) {
+      warnings(
+        "data_metrics_point and data_escape do not share the same
+      time period (not a single day in common). Please provide data sharing some
+      dates in common and comming from the same rasters."
+      )
+    }
+    if (shared < length(dates_point)) {
+      message(
+        "data_metrics_point covers more dates than data_escape, 
+      the merging will only be done on the days they share."
+      )
+    }
+    if (shared < length(data_escape)) {
+      message(
+        "data_escape covers more dates than data_metrics_point, 
+      the merging will only be done on the days they share."
+      )
+    }
+  }
+  if (check_1 == "TRUEFALSEFALSE") {
+    shared <- length(intersect(dates_morpho, dates_escape))
+    if (shared == 0) {
+      warnings(
+        "data_metrics_morpho and data_escape do not share the same
+      time period (not a single day in common). Please provide data sharing some
+      dates in common and comming from the same rasters."
+      )
+    }
+    if (shared < length(dates_morpho)) {
+      message(
+        "data_metrics_morpho covers more dates than data_escape, 
+      the merging will only be done on the days they share."
+      )
+    }
+    if (shared < length(data_escape)) {
+      message(
+        "data_escape covers more dates than data_metrics_morpho, 
+      the merging will only be done on the days they share."
+      )
+    }
+  }
+  if (check_1 == "FLASEFALSEFALSE") {
+    shared <- length(intersect(
+      intersect(dates_point, dates_morpho),
+      dates_escape
+    ))
+    if (shared == 0) {
+      warnings(
+        "The three dataset do not share a same time period (not a single day
+      in common). Please provide data sharing some dates in common and comming 
+      from the same rasters."
+      )
+    }
+    if (shared < length(dates_morpho)) {
+      message(
+        "data_metrics_morpho covers more dates than the others datasets, 
+      the merging will only be done on the days they share."
+      )
+    }
+    if (shared < length(data_escape)) {
+      message(
+        "data_escape covers more dates than the others datasets, 
+      the merging will only be done on the days they share."
+      )
+    }
+    if (shared < length(dates_point)) {
+      message(
+        "data_metrics_point covers more dates than the others datasets, 
+      the merging will only be done on the days they share."
+      )
+    }
+  }
+
+  ## Are the dataset spatially overlapping ?
+  ### data_metrics_point coordinates
+  data_metrics_point_xy <- data.table::rbindlist(data_metrics_point)
+  data_metrics_point_xy <- data.frame(
+    lon = data_metrics_point_xy$x,
+    lat = data_metrics_point_xy$y)
+  data_metrics_point_xy <- na.omit(unique(data_metrics_point_xy))
+  
+  ### data_metrics_morpho coordinates
+  data_metrics_morpho_xy <- data_metrics_morpho[[1]]
+  data_metrics_morpho_xy <- data.frame(
+    lon = data_metrics_morpho_xy$centroid_x,
+    lat = data_metrics_morpho_xy$centroid_y)
+  data_metrics_morpho_xy <- na.omit(unique(data_metrics_morpho_xy))
+  
+  ### data_escape coordinates
+  data_escape_xy <- data.frame(
+    lon = data_escape$from_x,
+    lat = data_escape$from_y)
+  data_escape_xy <- na.omit(unique(data_escape_xy))
+
+  ### Create polygones for each datasets :
+  #### Convert df to sf (using a metric crs)
+  sf_data_metrics_point_xy <- sf::st_as_sf(data_metrics_point_xy,
+  coords = c("lon","lat"),
+  crs=crs)
+  sf_data_metrics_morpho_xy <- sf::st_as_sf(data_metrics_morpho_xy,
+  coords = c("lon","lat"),
+  crs=crs)
+  sf_data_escape_xy <- sf::st_as_sf(data_escape_xy,
+  coords = c("lon","lat"),
+  crs=crs)
+  #### COmpute polygones
+  pol_data_metrics_point <- sf::st_concave_hull(sf_data_metrics_point_xy,
+  ratio = 0.8,
+  allow_holes = FALSE)
+  #### Test overlapping :
+
+  ########################## CODE ################################################
 }
