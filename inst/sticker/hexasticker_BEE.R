4714d39dd5b53c79835115af377efd91ab02324d
VicoDelannoy <valefvre@gmail.com>
2026-01-19 16:31:09 +0100

    empty file for creating hexsticker

---
+++ inst/sticker/hexasticker_BEE.R
@@ -0,0 +1,11 @@
+#'
+#' Create an Hexagonal Sticker for the BEE Package
+#'
+
+library(hexSticker)
+library(magick)
+library(sysfonts)
+library(tidyverse)
+
+
+# https://www.youtube.com/watch?v=O34vzdHOaEk
