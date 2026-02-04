f78d2418a13f0f9fae2fcaee5ccb9a9671f7385c
VicoDelannoy <valefvre@gmail.com>
2026-02-04 17:54:14 +0100

    I finished BEE.data.summarise

--- R/bee_data_summarise.R
+++ R/bee_data_summarise.R
@@ -6,8 +6,10 @@
 #' summarise metrics over different time periods: extreme events, weak periods,
 #' monthly periods and yearly periods.
 #'
-#'@param data
-#'@param variable description
+#'@param data the dataset conatainning the column you want to process (e.g. the
+#' ouput of BEE.data.merge())
+#'@param variable the name of the column for wich you want to compute mean,
+#' median etc.
 #'@param summarise_by takes the followings options:
 #' - "extreme_event" for each metric, the following will be computed for each
 #' extreme event: mean, median, variance, minimum and maximum, and the dates of
@@ -25,6 +27,7 @@
 #'
 #-------------------------------------------------------------------------------
 # data = merged_output ; variable = "perim_pixel_ratio" ; summarise_by = "extreme_event"
+# summarise_by = 3 ; variable = "azimut"
 
 BEE.data.summarise <- function(
   data = data,
@@ -34,12 +37,13 @@ BEE.data.summarise <- function(
   ########################## CHECKS ############################################
   # Check that the summarize_by is a valid option
   # ("extreme_event"/"weak"/"month"/"year") :
-  if (summarise_by != "extreme_event" | !is.numeric(summarise_by)) {
+  if (summarise_by != "extreme_event" & !is.numeric(summarise_by)) {
     warnings(
       "The summarise_by argument is not a suitable option, please 
-    provide one from the following list : extreme_event, day, weak, month, year"
+    provide 'extreme_event' or a number of time unit."
     )
   }
+
   if (is.list(data) == TRUE) {
     data <- do.call(rbind, data)
   }
@@ -134,43 +138,61 @@ BEE.data.summarise <- function(
   id <- colnames(data)[which(
     colnames(data) %in% c("ID_df_point", "ID_df_morpho", "ID_df_escape")
   )][1]
+
   if (summarise_by == "extreme_event") {
     ### Particular cases:
     if (variable == "azimut") {
       data$azimut_num <- as.numeric(data$azimut)
+      n_rows <- length(unique(data[, id]))
+      output <- data.frame(
+        ID = unique(data[, id]),
+        azimut_mean = circular::circular(rep(NA, n_rows)),
+        azimut_median = circular::circular(rep(NA, n_rows)),
+        azimut_sd = circular::circular(rep(NA, n_rows))
+      )
+
       data$azimut_circ <- circular::circular(
         data$azimut_num,
         units = "degrees",
         template = "geographics"
       )
+
       azimut_mean <- tapply(
         data$azimut_circ,
         data[, id],
         circular::mean.circular,
         na.rm = FALSE
       )
-      data$azimut_mean <- (azimut_mean[data[, id]] + 360) %% 360
+      azimut_mean <- (azimut_mean + 360) %% 360
+      output$azimut_mean <- azimut_mean[match(output$ID, names(azimut_mean))]
 
       azimut_median <- tapply(
         data$azimut_circ,
         data[, id],
         median_if_no_na
       )
-      data$azimut_median <- azimut_median[data[, id]]
+      azimut_median <- (azimut_median + 360) %% 360
+      output$azimut_median <- azimut_median[match(
+        output$ID,
+        names(azimut_median)
+      )]
 
       azimut_sd <- tapply(
         data$azimut_circ,
         data[, id],
         sd_if_no_na
       )
-      data$azimut_sd <- azimut_sd[data[, id]]
+      azimut_sd <- (azimut_sd + 360) %% 360
+      output$azimut_sd <- azimut_sd[match(output$ID, names(azimut_sd))]
     } else {
       output <- summarise_ID(data = data, variable = variable)
     }
+
     groups <- split(seq_len(nrow(data)), data[[id]])
     pixel_id <- lapply(groups, function(idx) {
       unique(sub("_.*", "", data[idx, id]))
     })
+
     output$pixel_id <- as.numeric(unlist(pixel_id))
     output <- split(output, output$pixel_id)
     return(output)
@@ -179,46 +201,82 @@ BEE.data.summarise <- function(
   if (is.numeric(summarise_by)) {
     ### Particular cases:
     if (variable == "azimut") {
-      # conversion of angle format to "circular"
       data$azimut_num <- as.numeric(data$azimut)
-      data$azimut_circ <- circular::circular(
-        data$azimut_num,
-        units = "degrees",
-        template = "geographics"
+      n_rows <- length(data[, id])
+      output <- data.frame(
+        ID = data[, id],
+        date = data[, "date"],
+        azimut = circular::circular(
+            data$azimut_num,
+            units = "degrees",
+            template = "geographics"
+          ),
+        data[, "azimut_num"],
+        azimut_mean = circular::circular(rep(NA, n_rows)),
+        azimut_median = circular::circular(rep(NA, n_rows)),
+        azimut_sd = circular::circular(rep(NA, n_rows)),
+        pixel_id = data[, "pixel_id"]
       )
-
-      data$azimut_mean <- NA
-      data$azimut_median <- NA
-      data$azimut_sd <- NA
+  
       for (i in seq(summarise_by, length(data[, variable]), 1)) {
         start <- i - summarise_by + 1
-        index <- start:i # indices à utiliser
-        data$azimut_mean[i] <- (circular::mean.circular(
-          data[index, "azimut_circ"],
-          na.rm = FALSE
-        ) +
-          360) %%
-          360
+        index <- start:i
 
-        if (any(is.na(data[index, "azimut_circ"]))) {
-          data$azimut_median[i] <- NA
-        } else {
-          data$azimut_median[i] <- (circular::median.circular(
-            data[index, "azimut_circ"],
-            na.rm = FALSE
-          ) +
-            360) %%
-            360
-        }
-        data$azimut_sd[i] <- (circular::sd.circular(
-          data[index, "azimut_circ"],
-          na.rm = FALSE
-        ) +
-          360) %%
-          360
+        output$azimut_mean[i] <- ifelse(
+          is.na(output[i, "azimut"]),
+          NA,
+          circular::mean.circular(
+            output[index, "azimut"],
+            na.rm = TRUE
+          )
+        )
+
+        output$azimut_median[i] <- ifelse(
+          is.na(output[i, "azimut"]),
+          NA,
+          circular::median.circular(
+            output[index, "azimut_circ"],
+            na.rm = TRUE
+          )
+        )
+
+        output$azimut_sd[i] <- ifelse(
+          is.na(output[i, "azimut"]),
+          NA,
+          circular::sd.circular(
+            output[index, "azimut_circ"],
+            na.rm = TRUE
+          )
+        )
       }
+      output$azimut_mean <- (circular::conversion.circular(
+        output$azimut_mean,
+        units = "degrees"
+      ) +
+        360) %%
+        360
+      output$azimut_median <- (circular::conversion.circular(
+        output$azimut_median,
+        units = "degrees"
+      ) +
+        360) %%
+        360
+      output$azimut_sd <- (circular::conversion.circular(
+        output$azimut_sd,
+        units = "degrees"
+      ) +
+        360) %%
+        360
+      output <- split(output, output$pixel_id)
+      return(output)
     } else {
-      output <- summarise_lag(data = data, variable = variable)
+      output <- summarise_lag(
+        data = data,
+        variable = variable,
+        lag_time = summarise_by
+      )
+      output <- split(output, output$pixel_id)
+      return(output)
     }
   }
 }
@@ -244,14 +302,14 @@ summarise_ID <- function(data = data, variable = "var_name") {
     function(f) {
       tapply(
         X = data[[variable]],
-        INDEX = data$ID_df_point,
+        INDEX = data[, id],
         FUN = f
       )
     }
   )
 
   output <- data.frame(
-    ID_df_point = names(res_list[[1]]),
+    ID = names(res_list[[1]]),
     do.call(
       cbind,
       lapply(
@@ -264,7 +322,7 @@ summarise_ID <- function(data = data, variable = "var_name") {
 
   names(output)[-1] <- paste0(variable, "_", names(funs))
 
-  List <- split(seq_len(nrow(data)), data$ID_df_point)
+  List <- split(seq_len(nrow(data)), data[, id])
   dates_max_min <- lapply(
     List,
     function(L_id) {
@@ -281,10 +339,10 @@ summarise_ID <- function(data = data, variable = "var_name") {
       }
     }
   )
-  date_max <- as.Date(sapply(date_max, function(x) x[[1]]))
+  date_max <- as.Date(sapply(dates_max_min, function(x) x[[1]]))
   date_min <- as.Date(sapply(dates_max_min, function(x) x[[2]]))
-  output$date_max <- date_max[output[, id]]
-  output$date_min <- date_min[output[, id]]
+  output[["date_max"]] <- date_max[match(output$ID, names(date_max))]
+  output[, "date_min"] <- date_min[match(output$ID, names(date_min))]
 
   return(output)
 }
@@ -315,63 +373,116 @@ sd_if_no_na <- function(x) {
 #' to be summarized PER TIME LAG
 #' @noRd
 #'
-summarise_lag <- function(data = data, variable = "var_name") {
+summarise_lag <- function(
+  data = data,
+  variable = "var_name",
+  lag_time = summarise_by
+) {
   data[[variable]] = as.numeric(data[[variable]])
-  funs <- list(
-    mean = function(x) mean(x, na.rm = TRUE),
-    median = function(x) median(x, na.rm = TRUE),
-    var = function(x) stats::var(x, na.rm = TRUE),
-    min = function(x) if (all(is.na(x))) NA_real_ else min(x, na.rm = TRUE),
-    max = function(x) if (all(is.na(x))) NA_real_ else max(x, na.rm = TRUE),
-    sum = function(x) sum(x, na.rm = TRUE)
-  )
-
-  res_list <- lapply(
-    funs,
-    function(f) {
-      tapply(
-        X = data[[variable]],
-        INDEX = data$ID_df_point,
-        FUN = f
+  output <- as.data.frame(
+    stats::setNames(
+      list(
+        data[id],
+        data$date,
+        data[, as.character(variable)],
+        NA_real_,
+        NA_real_,
+        NA_real_,
+        NA_real_,
+        NA_real_,
+        NA_real_,
+        as.Date(NA),
+        as.Date(NA),
+        data[["pixel_id"]]
+      ),
+      c(
+        "ID",
+        "date",
+        as.character(variable),
+        paste0(variable, "_mean"),
+        paste0(variable, "_median"),
+        paste0(variable, "_variance"),
+        paste0(variable, "_min"),
+        paste0(variable, "_max"),
+        paste0(variable, "_sum"),
+        paste0(variable, "_date_min"),
+        paste0(variable, "_date_max"),
+        "pixel_id"
       )
-    }
+    )
   )
 
-  output <- data.frame(
-    ID_df_point = names(res_list[[1]]),
-    do.call(
-      cbind,
-      lapply(
-        names(res_list),
-        function(nm) as.numeric(res_list[[nm]])
+  for (i in seq(lag_time, length(data[, variable]), 1)) {
+    start <- i - lag_time + 1
+    index <- start:i
+
+    output[i, paste0(as.character(variable), "_", "mean")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      mean(
+        data[index, variable],
+        na.rm = TRUE
       )
-    ),
-    row.names = NULL
-  )
+    )
+    output[i, paste0(as.character(variable), "_", "median")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      median(
+        data[index, variable],
+        na.rm = TRUE
+      )
+    )
+    output[i, paste0(as.character(variable), "_", "variance")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      stats::var(
+        data[index, variable],
+        na.rm = TRUE
+      )
+    )
 
-  names(output)[-1] <- paste0(variable, "_", names(funs))
+    output[i, paste0(as.character(variable), "_min")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      min(data[index, variable], na.rm = TRUE)
+    )
 
-  List <- split(seq_len(nrow(data)), data$ID_df_point)
-  dates_max_min <- lapply(
-    List,
-    function(L_id) {
-      data_mini <- data[L_id, ]
-      x <- data_mini[[as.character(variable)]] # <- ici
-      d <- data_mini$date
+    output[i, paste0(as.character(variable), "_max")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      max(data[index, variable], na.rm = TRUE)
+    )
 
-      if (all(is.na(x))) {
-        list(NA, NA)
-      } else {
-        d_max <- d[which.max(x)]
-        d_min <- d[which.min(x)]
-        list(d_max, d_min)
-      }
-    }
-  )
-  date_max <- as.Date(sapply(date_max, function(x) x[[1]]))
-  date_min <- as.Date(sapply(dates_max_min, function(x) x[[2]]))
-  output$date_max <- date_max[output[, id]]
-  output$date_min <- date_min[output[, id]]
+    output[i, paste0(as.character(variable), "_sum")] <- ifelse(
+      is.na(data[i, variable]),
+      NA,
+      sum(data[index, variable], na.rm = TRUE)
+    )
+
+    output[i, paste0(as.character(variable), "_", "date_min")] <- ifelse(
+      is.na(data[i, variable]),
+      as.Date(NA),
+      as.Date(data$date[
+        i -
+          which.min(
+            data[index, paste0(as.character(variable))]
+          ) +
+          1
+      ])
+    )
+
+    output[i, paste0(as.character(variable), "_", "date_max")] <- ifelse(
+      is.na(data[i, variable]),
+      as.Date(NA),
+      as.Date(data$date[
+        i -
+          which.max(
+            data[index, paste0(as.character(variable))]
+          ) +
+          1
+      ])
+    )
+  }
 
   return(output)
 }
