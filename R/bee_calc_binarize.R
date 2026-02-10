ed31c9c349296be33a69382143ab7528afb10674
VicoDelannoy <valefvre@gmail.com>
2026-02-10 10:31:15 +0100

    Update ROxygen of BEE.calc.binarize, try to increase picture size in vignettes

--- R/bee_calc_binarize.R
+++ R/bee_calc_binarize.R
@@ -1,34 +1,64 @@
 #' Binarise the spatraster
 #'
 #' @description
-#'  The function binarise a Spatraster by comparing the observed value with the 
-#' baseline. If "direction" argument is "above", the values above the baseline 
+#'  The function binarise a Spatraster by comparing the observed value with the
+#' baseline. If "direction" argument is "above", the values above the baseline
 #' will be converted to 1, and the value bellow baseline will be converted to 0.
 #' The process works in reverse when "direction" = below.
 #'
-#' @param YourSpatraster The Spatraster containing the values of the studied 
-#' parameter, with one layer per timestep.
-#' @param baseline A Spatraster built using the BEE.calc.baseline function that
-#'  contains the baseline value for each pixel on a given day of the year (with
-#'   366 layers).
-#' @param direction The accepted values for this argument are "above" or
-#'  "below". This tells the function whether it values above baseline or below
+#' @param YourSpatraster The Spatraster containing the values of the studied
+#' parameter, with one layer for each timestep.
+#' @param baseline A Spatraster built using the BEE.calc.baseline function,
+#' which contains the baseline value for each pixel on a given day of the year
+#' (with 366 layers), **or** a fixed threshold that you want to use as the baseline.
+#' The fixed threshold must be a number.
+#' @param direction The accepted values for this argument are *"above"* or
+#'  *"below"*. This tells the function whether it values above baseline or below
 #'   baseline are to be considered as extreme events.
 #'
-#' @return The function returns a list containing a Spatraster for each day of
-#'a typical year (366 Spatraster, with one layer per year provided in 
-#'YourSpatraster). These rasters have the same properties as "YourSpatraster" 
-#'but the values have been replace with 1 if the pixel value was more extreme 
-#'than the baseline and with 0 if it was not.
+#' @return The function returns a list containing a Spatraster for each day of a
+#'  typical year, providing a total of 366 Spatrasters, with one layer per year.
+#'  The first element of the list contains the rasters for all the first of
+#'  January included in the provided time series. These rasters have the same
+#'  properties as 'YourSpatraster', but the values have been replaced with 1 if
+#'  the pixel value was more extreme than the baseline, and with 0 if it was
+#'  not.
 #'
-#' @details For computational purposes in the next functions of the pipeline, 
-#' the order of the layers differs from that of "YourSpatraster". 
-#' BEE.calc.binarize is not design to work on 4D data (3D in space + time). 
-#' To do so, please refer to BEE.calc.4thD.
+#' @details For computational purposes in the next stages of the pipeline, the
+#'  order of the layers differs from that in 'YourSpatraster'.
+#'  BEE.calc.binarize is not designed to work with 4D data (3D in space and
+#'  time). To do so, please refer to BEE.calc.4thD.
+#'  This function only identifies days more extreme than a baseline, extra 
+#'  criteria to identify extreme event (e.g. minimum duration) will be apply in
+#'  the BEE.calc.true_event() function (next one in the framwork).
+#'
+#' @examples
+#' # Load data:
+#' ## Time series of sea surface temperature in the Gulf of Lion (France)
+#' # between January 2023 and 31 December 2025:
+#' file_name_1 <- system.file(file.path("extdata", "copernicus_example_data.nc"),
+#'                                   package = "BioExtremeEvent")
+#' copernicus_data <- terra::rast(file_name_1)
+#' ## Baseline of the sea surface temperature between January 1982 and December 
+#' # 2010 in the Gulf of Lion (France) computed using the BEE.calc.baseline()
+#' # function, with the following the arguments quantile_value = 0.9, 
+#' # time_window = 5, smooth_window = 7:
+#' file_name_2 <- system.file(file.path("extdata", "baseline_qt90"),
+#'                                   package = "BioExtremeEvent")
+#' baseline_qt90 <- terra::rast(file_name_2)
 #' 
+#' # Identify the days on which the values are higher than the baseline for the
+#' # 90th percentile.:
+#' binarized_EE <- BEE.calc.binarize(YourSpatraster=copernicus_data,
+#'                                   baseline=baseline_qt90,
+#'                                   direction = "above")
+#' # Identify days on which the value is lower than a fixed threshold.
+#' binarized_EE <- BEE.calc.binarize(YourSpatraster=copernicus_data,
+#'                                   baseline=12,
+#'                                   direction = "below")
+#'
 #' @export
-#' 
-
+#'
 #---------------------------------------------------------------------------
 # baseline <- baseline_qt90 ; direction <- "above"; YourSpatraster <- ds
 
@@ -39,19 +69,19 @@ BEE.calc.binarize <- function(YourSpatraster, baseline, direction) {
     # Change the extent to match that of YourSpatraster
     baseline <- terra::resample(baseline, YourSpatraster, method = "near")
     # Crop to obtain the same area as YourSpatraster
-    #baseline <- crop(baseline, new_extent, snap = "in") 
+    #baseline <- crop(baseline, new_extent, snap = "in")
     # Retrieve the dates
-    time_list_exp <- terra::time(YourSpatraster, format = "days") 
+    time_list_exp <- terra::time(YourSpatraster, format = "days")
     # Create dates in the format %m.%d for a leap year
-    time_list_base <- generate_month_day(2024) 
+    time_list_base <- generate_month_day(2024)
     names(YourSpatraster) <- time_list_exp # Rename the layers with the dates
     names(baseline) <- time_list_base # Rename the layers with the dates
     # Check if there is a fourth hidden dimension. For instance there could be
-    #several depths or several altitudes which would result in several layer 
+    #several depths or several altitudes which would result in several layer
     #having the same dates and thus the same length.
     if (any(table(terra::time(YourSpatraster)) != 1)) {
       #at least one date has several layers
-      rep <- sum(table(terra::time(YourSpatraster))) / 
+      rep <- sum(table(terra::time(YourSpatraster))) /
         length(table(terra::time(YourSpatraster)))
       if (rep == round(rep)) {
         # all dates are repeted the same number of time
@@ -66,39 +96,39 @@ BEE.calc.binarize <- function(YourSpatraster, baseline, direction) {
         for (d in seq(1, length(rast_list))) {
           delta[[d]] <- binarize_spat(rast_list[[d]], baseline, direction)
         }
-        # Binarize the pixels to 1 & 0 (function from the packacge itself, 
-        # see bellow) /!\ this part doesn't check if the value is above for 
+        # Binarize the pixels to 1 & 0 (function from the packacge itself,
+        # see bellow) /!\ this part doesn't check if the value is above for
         # five consecutive days or any duration
-        delta <- binarize_spat(YourSpatraster, baseline, direction) 
+        delta <- binarize_spat(YourSpatraster, baseline, direction)
         # Create a list of SpatRaster by date in the format %d.%m
-        delta_list <- RastToList(delta) 
+        delta_list <- RastToList(delta)
         return(delta_list) # Output the list
       }
     }
-    # Binarize the pixels to 1 & 0 (function from the packacge itself, 
-    # see bellow) /!\ this part doesn't check if the value is above for five 
+    # Binarize the pixels to 1 & 0 (function from the packacge itself,
+    # see bellow) /!\ this part doesn't check if the value is above for five
     # consecutive days or any duration
-    delta <- binarize_spat(YourSpatraster, baseline, direction) 
+    delta <- binarize_spat(YourSpatraster, baseline, direction)
     # Create a list of SpatRaster by date in the format %d.%m
-    delta_list <- RastToList(delta) 
+    delta_list <- RastToList(delta)
     return(delta_list) # Output the list
   }
-  
+
   if (is.numeric(baseline)) {
     # Retrieve the dates
-    time_list_exp <- terra::time(YourSpatraster, format = "days") 
+    time_list_exp <- terra::time(YourSpatraster, format = "days")
     # Rename the layers with the dates
-    names(YourSpatraster) <- time_list_exp 
+    names(YourSpatraster) <- time_list_exp
     if (direction == "above") {
       # Binarize the cells based on the condition
-      delta <- terra::ifel(YourSpatraster < baseline, 0, 1) 
+      delta <- terra::ifel(YourSpatraster < baseline, 0, 1)
     }
     if (direction == "bellow") {
       # Binarize the cells based on the condition
-      delta <- terra::ifel(YourSpatraster < baseline, 1, 0) 
+      delta <- terra::ifel(YourSpatraster < baseline, 1, 0)
     }
     # Create a list of SpatRaster by date in the format %d.%m
-    delta_list <- RastToList(delta) 
+    delta_list <- RastToList(delta)
     return(delta_list) # Output the list
   }
 }
@@ -109,10 +139,23 @@ BEE.calc.binarize <- function(YourSpatraster, baseline, direction) {
 #' @noRd
 
 generate_month_day <- function(year) {
-  days_per_month <- c(31, 28 + (year %% 4 == 0 &
-                      (year %% 100 != 0 |
-                      year %% 400 == 0)),
-                      31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
+  days_per_month <- c(
+    31,
+    28 +
+      (year %% 4 == 0 &
+        (year %% 100 != 0 |
+          year %% 400 == 0)),
+    31,
+    30,
+    31,
+    30,
+    31,
+    31,
+    30,
+    31,
+    30,
+    31
+  )
   months <- sprintf("%02d", rep(1:12, days_per_month))
   days <- sprintf("%02d", unlist(sapply(days_per_month, seq_len)))
   return(paste(months, days, sep = "."))
@@ -125,7 +168,7 @@ generate_month_day <- function(year) {
 
 binarize_spat <- function(obs, base, direction = "above") {
   #adjust dimension of 'base' so it matches those of 'obs'
-  dates <- as.Date(terra::time(obs))  # Dates
+  dates <- as.Date(terra::time(obs)) # Dates
   years <- as.integer(format(dates, "%Y")) # Years
   is_leap_year <- function(years) {
     return((years %% 4 == 0 & years %% 100 != 0) | (years %% 400 == 0))
@@ -154,7 +197,7 @@ binarize_spat <- function(obs, base, direction = "above") {
   terra::time(base_extended) <- dates
   #calculate anomalia
   delta <- obs - base_extended
-  
+
   if (direction == "above") {
     delta <- terra::ifel(delta <= 0, 0, 1)
   }
