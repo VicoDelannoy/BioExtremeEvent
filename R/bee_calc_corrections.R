7aa9a7ad425b47c4db218d33fbef577d9d8f7921
VicoDelannoy <valefvre@gmail.com>
2026-02-11 17:13:49 +0100

    publish calc_corrections vignette

--- R/bee_calc_corrections.R
+++ R/bee_calc_corrections.R
@@ -1,19 +1,25 @@
 #' Compute the impact of the selected definition of extreme events on the number
-#' of days of extreme event
+#' of days of extreme event.
 #'
 #' @description It computes the number of modifications between the raw
 #' binerised Spatraster and the Spartraster produced using the
-#' BEE.calc.true_event.
-#' This indicates of the differences in the number of days above the
-#' threshold/baseline and the number of days that are considered to be
-#' extreme events, according to the criteria set in BEE.calc.true_event.
-#' @param Events_corrected The second element of the BEE.calc.true_event ouput,
-#' which is a list of data.table containing information
-#' about the value of each pixel before and after definition criteria are
-#' applied to distinguish isolated extreme days from extreme events).
+#' *BEE.calc.true_event()*.
+#' @param true_event_raster The first element of the *BEE.calc.true_event()*
+#' ouput, which is a spatraster binarized that inclued the corrections by
+#' *BEE.calc.true_event()*.
+#' @param true_event_df_list The second element of the *BEE.calc.true_event()* ouput,
+#' which is a list of data.table containing information about the value of each
+#' pixel before and after definition criteria are applied to distinguish
+#' isolated extreme days from extreme events.
+#' @param plot Accepted values are TRUE of FALSE. Set to “FALSE” to not display
+#' the graphs.
 #'
-#' @returns A text summarising the modifications induced.
-#'
+#' @returns In R consol it returns a message with the number of 1 corrected to 0
+#' and the number of 0 corrected to 1. If plot=TURE it shows the cumulative
+#' percentage of changes through time.
+#' The object return if a dataframe with one line percombinaison of date and
+#' pixel_id. The dataframe has the same structure than the ones in
+#' true_event_df_list.
 #'
 #' @examples
 #' # to be added
@@ -21,156 +27,107 @@
 #' @export
 #'
 #-------------------------------------------------------------------------------
+# For tests : true_event_raster <- Corrected_rasters;
+# true_event_df_list <- Events_corrected
+BEE.calc.corrections <- function(
+  true_event_raster,
+  true_event_df_list,
+  plot = TRUE
+) {
+  df <- data.table::rbindlist(true_event_df_list)
+  # total changes in the spatraster
+  delta <- df$cleanned_value - df$original_value
+  n_positive <- sum(delta > 0, na.rm = TRUE) # 0 -> 1
+  n_negative <- sum(delta < 0, na.rm = TRUE) # 1 -> 0
+  # total changes in percentages (among non NA values) in the spatraster:
+  n_tot <- sum(!is.na(delta))
+  n_pos_per <- n_positive * 100 / n_tot
+  n_neg_per <- n_negative * 100 / n_tot
+  message(paste0(
+    "Number of 0 corrected to 1: ",
+    n_positive,
+    "\n",
+    "Number of 1 corrected to 0: ",
+    n_negative,
+    "\n",
+    "Percentage of non NA values corrected from 0 to 1: ",
+    n_pos_per,
+    "\n",
+    "Percentage of non NA values corrected from 1 to 0: ",
+    n_neg_per,
+    "\n",
+    n_pos_per + n_neg_per,
+    " % of the non NA values have been modified."
+  ))
+  # plot number of modifications trough time:
+  df$delta <- delta
+  df$one_to_zero <- ifelse(delta < 0, 1, 0)
+  df$zero_to_one <- ifelse(delta > 0, 1, 0)
+  if (plot == TRUE) {
+    # Number of corrections per date
+    sum_one_to_zero <- tapply(df$one_to_zero, df$date, sum, na.rm = TRUE)
+    sum_zero_to_one <- tapply(df$zero_to_one, df$date, sum, na.rm = TRUE)
+    # Cumulated sum
+    cum_one_to_zero_count <- cumsum(sum_one_to_zero)
+    cum_zero_to_one_count <- cumsum(sum_zero_to_one)
 
-BEE.calc.corrections <- function(Events_corrected) {
-  # Number of cores available
-  num_cores <- parallel::detectCores() - 2
-  # Calculate changes
-  results <- parallel::mclapply(
-    Events_corrected,
-    analyze_changes,
-    mc.cores = num_cores
-  )
-  results_df <- do.call(rbind, results)
-  rm(results)
+    dates <- names(sum_one_to_zero)
 
-  #Calculate percentages (data for the whole area and the whole time period)
-  abs_one_to_zero <- as.numeric(sum(results_df$one_to_zero, na.rm = TRUE))
-  abs_1 <- as.numeric(sum(results_df$total_ones, na.rm = TRUE))
-  abs_zero_to_one <- as.numeric(sum(results_df$zero_to_one, na.rm = TRUE))
-  abs_0 <- as.numeric(sum(results_df$total_zeros, na.rm = TRUE))
-  #percentage of 1 converted to 0 :
-  percentage_one_to_zero <- abs_one_to_zero * 100 / abs_1
-  percentage_zero_to_one <- abs_zero_to_one * 100 / abs_0
-
-  #Calculate mean per year and pixel
-  total_nb_info = sum(results_df$total_not_NA)
-  abs_one_to_zero_Ymean <- abs_one_to_zero * 100 / total_nb_info
-  abs_1_Ymean <- abs_1 / total_nb_info
-  # sd_1_Ymean <- as.numeric(stats::sd(results_df$total_ones, na.rm=TRUE)) /
-  # total_nb_info # variability of being 1 among the pixel
-  # corrections among the pixel :
-  sd_1to0_Ymean <- as.numeric(stats::sd(results_df$one_to_zero, na.rm = TRUE))
-  abs_zero_to_one_Ymean <- abs_zero_to_one / total_nb_info
-  abs_0_Ymean <- abs_0 / total_nb_info
-  # corrections among the pixel :
-  sd_1to0_Ymean <- as.numeric(stats::sd(results_df$zero_to_one, na.rm = TRUE))
-  cat(
-    "Absolut values for the whole area and whole time period :",
-    "\n",
-    "Original total number of days above threshold (1):",
-    "\n",
-    abs_1,
-    "\n",
-    "Percentage of 1 converted to 0:",
-    "\n",
-    percentage_one_to_zero,
-    "%",
-    ", (percentage among all initial '1' values)",
-    "\n",
-    "Original total number of days below threshold (0):",
-    "\n",
-    abs_0,
-    "\n",
-    "Percentage of 0 converted to 1:",
-    "\n",
-    percentage_zero_to_one,
-    "%",
-    "\n",
-    "\n",
-    "Mean values per pixel and date :",
-    "\n",
-    "Mean percentage of pixel value above threshold :",
-    "\n",
-    abs_1_Ymean * 100,
-    "%",
-    "\n",
-    "Mean percentage of corrections from 1 to 0:",
-    "\n",
-    abs_one_to_zero_Ymean,
-    "%",
-    "(percentage among all values (1 & 0))",
-    "\n",
-    "Standard deviation of corrections among pixel(1 to 0):",
-    "\n",
-    sd_1to0_Ymean,
-    "%",
-    "\n",
-    "Mean percentage of pixel value bellow thershold :",
-    "\n",
-    abs_0_Ymean * 100,
-    "%",
-    "\n",
-    "Mean percentage of corrections to 1 ",
-    "\n",
-    abs_zero_to_one_Ymean,
-    "%",
-    "\n",
-    "Standard deviation of corrections among pixel (0 to 1)",
-    "\n",
-    sd_1to0_Ymean,
-    "%",
-    "\n",
-    "\n",
-    "NA values in your dataset :",
-    "\n",
-    "Total number of NA values :",
-    sum(results_df$total_NA),
-    "\n",
-    "Total number of pixel that are always NA :",
-    sum(sapply(Events_corrected, function(df) {
-      all(is.na(df$original_value))
-    })),
-    "\n",
-    "Total number of NA values excluding those from pixels that are 
-    always NA :",
-    # total NA value - number of pixels that alwayes are NA * number of days
-    # in the time serie :
-    sum(results_df$total_NA) -
-      sum(sapply(Events_corrected, function(df) {
-        all(is.na(df$original_value))
-      })) *
-        length(Events_corrected[[1]]$original_value),
-    "\n"
-  )
-}
-
-#' Check modifications
-#' 
-#' @noRd
-
-analyze_changes <- function(dataframe) {
-  # Count modifications
-  one_to_zero <- sum(
-    dataframe$original_value == 1 &
-      dataframe$cleanned_value == 0,
-    na.rm = TRUE
-  )
-  zero_to_one <- sum(
-    dataframe$original_value == 0 &
-      dataframe$cleanned_value == 1,
-    na.rm = TRUE
-  )
-
-  # Count orginal total value
-  total_ones <- sum(dataframe$original_value == 1, na.rm = TRUE)
-  total_zeros <- sum(dataframe$original_value == 0, na.rm = TRUE)
-
-  # Count NA
-  total_NA <- sum(is.na(dataframe$original_value))
-  total_not_NA <- sum(!is.na(dataframe$original_value))
-  tot_value <- total_not_NA + total_NA
-
-  return(
-    data.frame(
-      one_to_zero = one_to_zero,
-      zero_to_one = zero_to_one,
-      total_ones = total_ones,
-      total_zeros = total_zeros,
-      total_NA = total_NA,
-      total_not_NA = total_not_NA,
-      tot_value = tot_value
+    # Percentage cumulated and df for plot
+    df_plot <- data.frame(
+      date = as.Date(names(sum_one_to_zero)),
+      cum_one_to_zero = 100 * cum_one_to_zero_count / n_tot,
+      cum_zero_to_one = 100 * cum_zero_to_one_count / n_tot
     )
-  )
-}
 
+    cumulative_changes <- ggplot2::ggplot(df_plot) +
+      ggplot2::geom_line(
+        ggplot2::aes(
+          x = date,
+          y = cum_one_to_zero,
+          color = "1 -> 0",
+          group = 1
+        ),
+        size = 1
+      ) +
+      ggplot2::geom_line(
+        ggplot2::aes(
+          x = date,
+          y = cum_zero_to_one,
+          color = "0 -> 1",
+          group = 1
+        ),
+        size = 1
+      ) +
+      ggplot2::scale_color_manual(
+        name = "Type of change",
+        values = c("1 -> 0" = "blue", "0 -> 1" = "red")
+      ) +
+      ggplot2::scale_x_date(
+        date_breaks = "1 year",
+        date_labels = "%Y"
+      ) +
+      ggplot2::labs(
+        x = "Year",
+        y = "Cumulative percentage change",
+        title = "Cumulative percentage of changes by date among non NA values."
+      ) +
+      ggplot2::theme_minimal(base_size = 14) +
+      ggplot2::theme(
+        axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)
+      )
+    print(cumulative_changes)
+    # Sum of changes per pixel trough all the time period:
+    sum_delta <- tapply(df$delta, df$pixel_id, sum, na.rm = TRUE)
+    shell <- true_event_raster[[1]]
+    vals <- terra::values(shell)
+    pos <- as.integer(names(sum_delta))
+    vals[pos] <- sum_delta
+    terra::values(shell) <- vals
+    terra::plot(
+      shell,
+      main = "Number of extreme days added or withdrawn per pixel."
+    )
+  }
+  return(df)
+}
