fd7769b40a148af51de62b14d420e981be6893d0
VicoDelannoy <valefvre@gmail.com>
2026-05-13 15:00:47 +0200

    Add sticker

--- inst/sticker/hexasticker_BEE.R
+++ inst/sticker/hexasticker_BEE.R
@@ -2,10 +2,28 @@
 #' Create an Hexagonal Sticker for the BEE Package
 #'
 
-library(hexSticker)
-library(magick)
-library(sysfonts)
-library(tidyverse)
-
+#library(hexSticker)
+#library(magick)
+#library(sysfonts)
+#library(tidyverse)
 
 # https://www.youtube.com/watch?v=O34vzdHOaEk
+
+mhw_image <- magick::image_read("inst/sticker/mhw_time_2D.png")
+
+hexSticker::sticker(
+  subplot = mhw_image,
+  package = "BioExtremeEvent",
+  s_width = 1.7,
+  s_height = 1.8,
+  s_x = 0.99,
+  s_y = 0.82,
+  p_size = 6.5,
+  p_y = 1.33,
+  p_color = "#020002ff",
+  h_fill = "#48d4325e",
+  h_color = "black",
+  h_size = 1,
+  #url = "https://vicodelannoy.github.io/BioExtremeEvent/",
+) |>
+  print()
